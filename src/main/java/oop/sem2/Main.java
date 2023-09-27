8e5a83084dac4982244c1a4271af1c46eed15847
alexf <rassylka82@inbox.ru>
2023-09-27 20:20:03 +0300

    ООП ДЗ-2

---
+++ src/main/java/oop/sem2/Main.java
@@ -0,0 +1,16 @@
+package oop.sem2;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Cat cat = new Cat("Tom");
+        System.out.println(cat.getName());
+        List<String> list = new LinkedList<>();
+
+
+
+    }
+}
