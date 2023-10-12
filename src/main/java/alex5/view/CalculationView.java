d1eb06a83901bc263cbeda3e9b8322393d6690ee
alexf <rassylka82@inbox.ru>
2023-10-12 22:12:36 +0300

    oop-hw-5,6

---
+++ src/main/java/alex5/view/CalculationView.java
@@ -0,0 +1,10 @@
+package alex5.view;
+
+public class CalculationView {
+
+    /** printResult() - Функция выводит result в консоль
+     */
+    public void printResult(String result) {
+        System.out.println(result);
+    }
+}
