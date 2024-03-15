3210920592b5b318a2250dd61966d25738e6443c
alexf <rassylka82@inbox.ru>
2024-03-15 18:52:39 +0300

    ДЗ-3.

---
+++ src/main/java/hw3/MainApp.java
@@ -0,0 +1,89 @@
+package hw3;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+        List<Worker> workers = new ArrayList<>(List.of(
+           new Worker("Горбачёв Михаил Сергеевич",  "помощник комбайнёра",
+                   "666666", 200, LocalDate.of(1931, 8, 30)),
+           new Worker("Черненко Константин Устинович", " зав. отделом пропаганды и агитации",
+                   "555555", 190, LocalDate.of(1911, 9,11)),
+           new Worker("Андропов Юрий Владимирович", "матрос",
+                   "444444", 180, LocalDate.of(1914, 6, 15)),
+           new Worker("Брежнев Леонид Ильич", "землемер-землеустроитель",
+                   "333333", 170, LocalDate.of(1906, 12, 19)),
+           new Worker("Хрущёв Никита Сергеевич", "инструктор политотдела 9-й Кубанской армии",
+                   "222222", 160, LocalDate.of(1894, 4, 15))
+        ));
+
+        // Добавляем руководителя.
+        workers.add(new Manager("Джугашвили Иосиф Виссарионович", " секретарь ЦК ВКП(б) — КПСС (1934—1953)",
+                "111111", 150, LocalDate.of(1878, 12, 18)));
+
+        // Сортировка по возрасту по встроенному компаратору.
+        Collections.sort(workers);
+        printAll(workers);
+
+        // Распечатка с сортировкой по ЗП ДО повышения.
+        printAll(workers.stream().sorted(Comparator.comparingInt(Worker::getSalary)).collect(Collectors.toList()));
+        // Повышение зарплаты.
+        Manager.raiseSalary(workers, 110, 100);
+        // Распечатка с сортировкой по ЗП ПОСЛЕ повышения.
+        printAll(workers.stream().sorted(Comparator.comparingInt(Worker::getSalary)).collect(Collectors.toList()));
+
+        // Средние возраст и ЗП.
+        System.out.println(averageAge(workers));
+        System.out.println(averageSalary(workers));
+
+        // Сортировка по алфавиту.
+        workers.sort(Comparator.comparing(Worker::getSurname));
+        printAll(workers);
+
+        // Находим в массиве руководителя.
+        Manager manager = (Manager) workers.stream().filter(w -> w instanceof Manager).findFirst()
+                .orElseThrow(() -> new RuntimeException("Manager not found."));
+        System.out.println(manager);
+        System.out.println();
+
+        // Выдаём всем работникам задания.
+        int index = 1;
+        for (Worker worker : workers) {
+            if (!(worker instanceof Manager)) {
+                manager.assign(worker, new Task(String.format("Task #%d", index++)));
+            }
+        }
+        for (Worker worker : workers) {
+            System.out.printf("%s, %s%n", worker.getSurname(), worker.getTask());
+        }
+    }
+
+    private static void printAll(List<Worker> workers) {
+        for (Worker worker : workers) {
+            System.out.println(worker);
+        }
+        System.out.println("================================================");
+    }
+
+    private static float averageAge(List<Worker> workers) {
+        int sumAge = 0;
+        for (Worker worker : workers) {
+            sumAge += worker.getAge();
+        }
+        return (float) sumAge / workers.size();
+    }
+
+    private static float averageSalary(List<Worker> workers) {
+        int sumSalary = 0;
+        for (Worker worker : workers) {
+            sumSalary += worker.getSalary();
+        }
+        return (float) sumSalary / workers.size();
+    }
+}
---
+++ src/main/java/hw3/Worker.java
@@ -0,0 +1,116 @@
+package hw3;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+public class Worker implements Comparable<Worker> {
+    private String surname;
+    private String name;
+    private String patronymic;
+    private String position;
+    private String phone;
+    private int salary;
+    private LocalDate birthdate;
+    private Task task;
+
+    public Worker(String fio, String position, String phone, int salary, LocalDate birthdate) {
+        String[] arr = fio.strip().split(" ");
+        this.surname = arr[0];
+        this.name = arr[1];
+        this.patronymic = arr[2];
+        this.position = position;
+        this.phone = phone;
+        this.salary = salary;
+        this.birthdate = birthdate;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public Task getTask() {
+        return task;
+    }
+
+    public void assign(Task task) {
+        this.task = task;
+    }
+
+    public int getAge() {
+        if (this.birthdate == null) {
+            return -1;
+        }
+        return difDate(this.birthdate);
+    }
+
+    public void setBithdate(LocalDate birthdate) {
+        if (difDate(birthdate) >= 14) {
+            this.birthdate = birthdate;
+        } else {
+            this.birthdate = null;
+        }
+    }
+
+    private int difDate(LocalDate date) {
+        return (int) date.until(LocalDate.now(), ChronoUnit.YEARS);
+    }
+
+    @Override
+    public String toString() {
+        return "Worker{" + surname + " " +  name + " " + patronymic + '\'' +
+                ", " + position +
+                ", phone='" + phone + '\'' +
+                ", salary=" + salary +
+                ", age=" + this.getAge() +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Worker o) {
+        return this.getAge() - o.getAge();
+    }
+}
