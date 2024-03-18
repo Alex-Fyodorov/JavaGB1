eaca8e64b5f474f1fdcea3d247f826f4fab4e9b2
alexf <rassylka82@inbox.ru>
2024-03-18 21:11:50 +0300

    Семинар4

---
+++ src/main/java/javaone/sem4/market/exceptions/QuantityIsNegativeException.java
@@ -0,0 +1,7 @@
+package javaone.sem4.market.exceptions;
+
+public class QuantityIsNegativeException extends Exception {
+    public QuantityIsNegativeException(String message) {
+        super(message);
+    }
+}
