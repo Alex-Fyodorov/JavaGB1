db527bc872543f489ccb1fe494d49a8072fc4a1c
Alex <rassylka82@inbox.ru>
2025-05-18 16:34:49 +0300

    Распихал крестики-нолики по пакетам

--- src/main/java/others/Kapreckar.java
+++ src/main/java/others/Kapreckar.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 
 public class Kapreckar {
 
+    // Программа, иллюстрирующая закон Капрекара.
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String str = scanner.nextLine();
